1201e66d70947fa8bee0e310c5eeac2906afaa32
somefriggnidiot <somefriggnidiot@gmail.com>
2017-12-14 22:22:56 -0600

    CONBOT-2 Add annotations

--- src/main/conf/ConnectionConfiguration.java
+++ src/main/conf/ConnectionConfiguration.java
@@ -21,66 +21,82 @@ public final class ConnectionConfiguration {
    @JsonProperty("bot-slow-mode")
    private String botSlowMode;
 
+   @JsonProperty("instance-name")
    public String getInstanceName() {
       return instanceName;
    }
 
+   @JsonProperty("instance-name")
    public void setInstanceName(String instanceName) {
       this.instanceName = instanceName;
    }
 
+   @JsonProperty("server-address")
    public String getServerAddress() {
       return serverAddress;
    }
 
+   @JsonProperty("server-address")
    public void setServerAddress(String serverAddress) {
       this.serverAddress = serverAddress;
    }
 
+   @JsonProperty("server-query-port")
    public int getServerQueryPort() {
       return serverQueryPort;
    }
 
+   @JsonProperty("server-query-port")
    public void setServerQueryPort(int serverQueryPort) {
       this.serverQueryPort = serverQueryPort;
    }
 
+   @JsonProperty("virtual-server-id")
    public int getVirtualServerId() {
       return virtualServerId;
    }
 
+   @JsonProperty("virtual-server-id")
    public void setVirtualServerId(int virtualServerId) {
       this.virtualServerId = virtualServerId;
    }
 
+   @JsonProperty("server-query-name")
    public String getServerQueryName() {
       return serverQueryName;
    }
 
+   @JsonProperty("server-query-name")
    public void setServerQueryName(String serverQueryName) {
       this.serverQueryName = serverQueryName;
    }
 
+   @JsonProperty("server-query-password")
    public String getServerQueryPassword() {
       return serverQueryPassword;
    }
 
+   @JsonProperty("server-query-password")
    public void setServerQueryPassword(String serverQueryPassword) {
       this.serverQueryPassword = serverQueryPassword;
    }
 
+   @JsonProperty("bot-nickname")
    public String getBotNickname() {
       return botNickname;
    }
 
+   @JsonProperty("bot-nickname")
    public void setBotNickname(String botNickname) {
       this.botNickname = botNickname;
    }
 
+   @JsonProperty("bot-slow-mode")
    public String getBotSlowMode() {
       return botSlowMode;
    }
 
+   @JsonProperty("bot-slow-mode")
    public void setBotSlowMode(String botSlowMode) {
       this.botSlowMode = botSlowMode;
    }
