6ca800e891d6f61d6b1cc3131119c1b77f3a88a5
somefriggnidiot <somefriggnidiot@gmail.com>
2018-01-22 19:08:22 -0600

    INCOBOT-15 #15 Add javadocs to config.

--- src/main/conf/IdleCheckConfiguration.java
+++ src/main/conf/IdleCheckConfiguration.java
@@ -1,7 +1,11 @@
 package main.conf;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 
+/**
+ * DTO for idle-checker configuration.
+ */
 public class IdleCheckConfiguration {
    @JsonProperty("idle-max-time-minutes")
    private int maxTimeMinutes;
@@ -10,30 +14,76 @@ public class IdleCheckConfiguration {
    private int destinationChannel;
 
    @JsonProperty("idle-ignore-groups")
-   private int[] ignoreGroups;
+   private List<Integer> ignoreGroups;
 
+   /**
+    * @return the maximum time in minutes a user may remain idle before being moved.
+    */
+   @JsonProperty("idle-max-time-minutes")
    public int getMaxTimeMinutes() {
       return maxTimeMinutes;
    }
 
+   /**
+    * @param maxTimeMinutes the maximum time in minutes a user may remain idle before being moved.
+    */
+   @JsonProperty("idle-max-time-minutes")
    public void setMaxTimeMinutes(int maxTimeMinutes) {
       this.maxTimeMinutes = maxTimeMinutes;
    }
 
+   /**
+    * @return the ID of the channel idle users will be moved to.
+    */
+   @JsonProperty("idle-destination-channel")
    public int getDestinationChannel() {
       return destinationChannel;
    }
 
+   /**
+    * @param destinationChannel the ID of the channel idle users will be moved to.
+    */
+   @JsonProperty("idle-destination-channel")
    public void setDestinationChannel(int destinationChannel) {
       this.destinationChannel = destinationChannel;
    }
 
-   public int[] getIgnoreGroups() {
+   /**
+    * @return a list of group IDs for groups that will be exempt from the idle checker.
+    */
+   @JsonProperty("idle-ignore-groups")
+   public List<Integer> getIgnoreGroups() {
       return ignoreGroups;
    }
 
-   public void setIgnoreGroups(int[] ignoreGroups) {
+   /**
+    * @param ignoreGroups a list of group IDs for groups that will be exempt from the idle checker.
+    */
+   @JsonProperty("idle-ignore-groups")
+   public void setIgnoreGroups(List<Integer> ignoreGroups) {
       this.ignoreGroups = ignoreGroups;
+   }
+
+   /**
+    * @param ignoreGroup a single group ID to add to the list of groups exempt from the idle
+    * checker.
+    */
+   @JsonProperty("idle-ignore-groups")
+   public void setIgnoreGroup(int ignoreGroup) {
+      if (!this.ignoreGroups.contains(ignoreGroup)) {
+         ignoreGroups.add(ignoreGroup);
+      }
+   }
+
+   /**
+    * @param ignoreGroup a single group ID to remove from the list of groups exempt from the idle
+    * checker.
+    */
+   @JsonProperty("idle-ignore-groups")
+   public void removeIgnoreGroup(int ignoreGroup) {
+      if (this.ignoreGroups.contains(ignoreGroup)) {
+         ignoreGroups.remove(Integer.valueOf(ignoreGroup));
+      }
    }
 
 }
