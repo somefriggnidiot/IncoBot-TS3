b3a8446155a3674be457d3bae6a0387030181f8a
somefriggnidiot <somefriggnidiot@gmail.com>
2018-04-09 20:40:52 -0500

    INCOBOT-13 Add javadocs for database connector

--- src/main/data_access/DatabaseConnector.java
+++ src/main/data_access/DatabaseConnector.java
@@ -4,11 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Handles connections to the embedded database.
+ */
 public class DatabaseConnector {
    private static String USER_TABLE = "./db/users.odb";
    private EntityManagerFactory managerFactory;
    private EntityManager manager;
 
+   /**
+    * Generates an {@link EntityManager} for the given {@link Table}
+    *
+    * @param table the {@code Table} with which a connection should be established.
+    * @return an {@code EntityManager} that manages transactions to the provided {@code Table}.
+    */
    public EntityManager getEntityManager(Table table) {
       if (table == Table.USER) {
          this.managerFactory = Persistence.createEntityManagerFactory(USER_TABLE);
@@ -18,11 +27,17 @@ public class DatabaseConnector {
       return manager;
    }
 
+   /**
+    * Closes the connection this {@link DatabaseConnector} is utilizing.
+    */
    public void close() {
       manager.close();
       managerFactory.close();
    }
 
+   /**
+    * Existing database tables in use by this program.
+    */
    public enum Table {
       USER
    }
